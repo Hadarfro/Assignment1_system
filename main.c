cee74e0baf73aff2febb76f059a14c3a73570d1a
hadar <hadarfro12@gmail.com>
2024-01-28 22:16:27 +0200

    fixing main

--- main.c
+++ main.c
@@ -13,6 +13,22 @@ int main(){
 
     scanf("%d %d", &num1 , &num2);
 
+printf("the Armstrong numbers are:");
+     for (int i = num1; i <= num2; ++i) {
+        if (isArmstrong(i)) {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
+
+//palindrome numbers between num1 and num2
+    printf("the Palindrome is:");
+    for (int i = num1; i <= num2; ++i) {
+        if (isPalindrome(i)) {
+            printf(" %d", i);
+        }
+    }
+    printf("\n");
 
     // Prime numbers between num1 and num2
     printf("the prime numbers are:");
@@ -33,39 +49,12 @@ int main(){
     }
     printf("\n");
     
-    //palindrome numbers between num1 and num2
-    printf("the Palindrome is:");
-    for (int i = num1; i <= num2; ++i) {
-        if (isPalindrome(i)) {
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
+    
     
 
-    printf("the Armstrong numbers are:");
-     for (int i = num1; i <= num2; ++i) {
-        if (isArmstrong(i)) {
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
-
-    printf("the ArmstrongRec numbers are:");
-     for (int i = num1; i <= num2; ++i) {
-        if (isArmstrongRec(i)) {
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
     
-printf("the PalindromeRec is:");
-    for (int i = num1; i <= num2; ++i) {
-        if (isPalindromeRec(i)) {
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
+
+   
 return 0;
 
 }
